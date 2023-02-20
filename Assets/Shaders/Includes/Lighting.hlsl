26f831df5c49bfc89449bd08a0bc911395e007ac
hnguyenngoc <hieu.nguyen-ngoc@ubisoft.com>
2023-02-20 17:45:51 +0700

    first

---
+++ Assets/Shaders/Includes/Lighting.hlsl
@@ -0,0 +1,24 @@
+#ifndef CUSTOM_LIGHTING_INCLUDED
+#define CUSTOM_LIGHTING_INCLUDED
+
+void CalculateMainLight_float(float3 WorldPos, out float3 Direction, out float3 Color) {
+#if SHADERGRAPH_PREVIEW
+    Direction = float3(0.5,0.5,0);
+    Color = 1;
+#else
+    Light mainLight = GetMainLight(0);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+#endif
+}
+
+#endif
+
+// #if defined(SHADERGRAPH_PREVIEW)
+//     Direction = half3(0.5, 0.5, 0);
+//     Color = 1;
+// #else
+//     Light light = GetMainLight();
+//     Direction = light.direction;
+//     Color = light.color;
+// #endif
