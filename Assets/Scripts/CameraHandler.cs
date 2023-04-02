55e3747c440614d072fdff982415197c28ceaf1d
uyvaquoc123 <hieuzzai9@gmail.com>
2023-04-03 02:20:39 +0700

    feat: rotate camera around

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour
+{
+    public bool isRotating = false;
+
+    [SerializeField] private float rotationSpeed = 0.2f;
+    private Vector3 _prevPosition;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            _prevPosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = Input.mousePosition - _prevPosition;
+            
+            transform.Rotate(0, direction.x * rotationSpeed, 0);
+            
+            _prevPosition = Input.mousePosition;
+        }
+    }
+
+
+}
